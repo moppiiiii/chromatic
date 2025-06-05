bf130766cc2801af2194063ff4804890900438d8
moppiiiii <tnktmhssrc@gmail.com>
2025-06-05 12:19:20 +0900

    setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,10 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
-const isVRT = process.env.STORYBOOK_TARGET === 'vrt';
-
 const config: StorybookConfig = {
-  "stories": isVRT
-  ? ['../src/**/*.vrt.stories.@(ts|tsx|js|jsx)']
-  : ['../src/**/!(*.vrt).stories.@(ts|tsx|js|jsx)'],
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/!(*.vrt).stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   "addons": [
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
